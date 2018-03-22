341e6a6a4649531ee2291a85ca3ede2005a3cf13
chaurasiat <35598251+chaurasiat@users.noreply.github.com>
2018-03-22 13:31:02 +0530

    final

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -64,6 +64,7 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
     eq1 = sqrt(px * px + py * py);
   }
   float eq2 = atan2(py,px);
+  
   float eq3 = (px*vx+py*vy)/eq1;
     
   //Feed in equations above
@@ -71,6 +72,14 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   H_func << eq1, eq2, eq3;
     
   VectorXd y = z - H_func;
+  if(y(1)>M_PI)
+        y(1)-=2*M_PI 
+    else if(y(1)<=-M_PI)
+	     
+        y(1)+=2*M_PI 
+         
+
+
   MatrixXd Ht = H_.transpose();
   MatrixXd S = H_ * P_ * Ht + R_;
   MatrixXd Si = S.inverse();
